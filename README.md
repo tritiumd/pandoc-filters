3c75e1a5f030b809d77606ed23f0ce1071e1b2e0
ngocptblaplafla <phanthanhngocblaplafla@gmail.com>
2024-06-12 19:10:40 +0700

    add filter manager, credit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pandoc-filters
 Pandoc filters collection used in pandoc-texlive-full
+# Credits
+- [Pandoc](https://github.com/jgm/pandoc)
+- [Pandoc Team](https://github.com/pandoc) especially [Tarleb](https://tarleb.com) for help me a lot
+- [Pandoc-Ext Team](https://github.com/pandoc-ext)
+- chrisaga for some [filter](https://github.com/chrisaga/hk-pandoc-filters)
+- And many answers from Github and StackOverflow I forgot
---
+++ filter-manager.sh
@@ -0,0 +1,37 @@
+redownload=false
+function downloadFilterPandocExt() {
+  REPO="https://raw.githubusercontent.com/pandoc-ext"
+  EXT_NAME=$1
+  EXT_VERSION=$2
+  redownload=$3
+  if [ ! -f ${EXT_NAME}.lua ] || [ $redownload = true ]
+  then
+      wget ${REPO}/${EXT_NAME}/${EXT_VERSION}/_extensions/${EXT_NAME}/${EXT_NAME}.lua -O ${EXT_NAME}.lua
+  fi
+}
+
+# Install pandoc lua filter
+downloadFilterPandocExt diagram v1 $redownload
+downloadFilterPandocExt multibib dd7de577e8e9ebb58f13edc3f7615141552b02c5 $redownload
+downloadFilterPandocExt multibib main $redownload
+
+function downloadFilter() {
+  REPO=$1
+  EXT_NAME=$2
+  EXT_VERSION=$3
+  redownload=$4
+  if [ ! -f ${EXT_NAME}.lua ] || [ $redownload = true ]
+  then
+      wget ${REPO}/${EXT_VERSION}/${EXT_NAME}/${EXT_NAME}.lua -O ${EXT_NAME}.lua
+  fi
+}
+
+REPO="https://raw.githubusercontent.com/pandoc/lua-filters/"
+downloadFilter $REPO include-code-files master $redownload
+downloadFilter $REPO include-files master $redownload
+downloadFilter $REPO abstract-to-meta master $redownload
+downloadFilter $REPO author-info-blocks master $redownload
+
+REPO="https://raw.githubusercontent.com/chrisaga/hk-pandoc-filters/main/"
+downloadFilter $REPO tables-rules master $redownload
+downloadFilter $REPO column-div master $redownload
